081c93f3eb0c97808cfdafda845075fde6f80698
Mariusz <ulejczykm@gmail.com>
2020-09-15 22:52:47 +0200

    more readable variables, li height set to 70%, Next button

--- src/components/sliderView.vue
+++ src/components/sliderView.vue
@@ -5,7 +5,7 @@
                     <img :src="`http://source.unsplash.com/${photo}`" class="imgInSlide" alt="">  
             </li>
         </ul>
-        <div @click="nextImages(url.length)">button</div>
+        <button id="send" @click="nextImages()">Next</button>
     </main>
 </template>
 
@@ -24,66 +24,66 @@ export default {
     startFetch(){
           
         const arrayOfSlugs = new Array();  
-        var newArr = new Array();
+        const displayedSlugs = new Array();
 
         fetch('https://picsum.photos/v2/list')
         .then(res => {
             return res.json()
         })
         .then(data =>  {
-            const arrayOfURL = data.map(photo => {
+            const arrayOfURLs = data.map(photo => {
                 return photo.url;
             });
            
-            for (let i = 0; i < arrayOfURL.length; i++) {
-                let el = arrayOfURL[i];
-                let slug = el.slice(el.lastIndexOf("/") + 1); 
+            for (let i = 0; i < arrayOfURLs.length; i++) {
+                let fullURL = arrayOfURLs[i];
+                let slug = fullURL.slice(fullURL.lastIndexOf("/") + 1); 
                 arrayOfSlugs.push(slug);
             }  
             for (let i = 0; i < 3; i++) {
-                 let newElement = arrayOfSlugs.shift();
-                 newArr.push(newElement);
+                 let newSlug = arrayOfSlugs.shift();
+                 displayedSlugs.push(newSlug);
             }
-            this.url =  newArr;
+            this.url = displayedSlugs;
             this.restOfArray = arrayOfSlugs;
-            console.log(newArr)
-            console.log( this.restOfArray)
+    /*         console.log(displayedSlugs)
+            console.log( this.restOfArray) */
             
         })
         .catch(error => console.log(error))
     },
     nextImages(){
         const arrayOfSlugs = this.restOfArray;  
-        const newArr = [];
+        const displayedSlugs = [];
  
-            for (let i = 0; i < 3; i++) {
-                 let newElement = arrayOfSlugs.shift();
-                 newArr.push(newElement);
-            }
-            this.url =  newArr;
-            this.restOfArray = arrayOfSlugs;
-            console.log(newArr)
-            console.log(arrayOfSlugs)
+        for (let i = 0; i < 3; i++) {
+                let newElement = arrayOfSlugs.shift();
+                displayedSlugs.push(newElement);
+        }
+        this.url = displayedSlugs;
+        this.restOfArray = arrayOfSlugs;
+     /*    console.log(displayedSlugs)
+        console.log(arrayOfSlugs) */
 
-            if (arrayOfSlugs.length <=0){
-                fetch('https://picsum.photos/v2/list')
-                .then(res => {
-                    return res.json()
-                })
-                .then(data =>  {
-                    const arrayOfURL = data.map(photo => {
-                        return photo.url;
-                    });
-                
-                    for (let i = 0; i < arrayOfURL.length; i++) {
-                        let el = arrayOfURL[i];
-                        let slug = el.slice(el.lastIndexOf("/") + 1); 
-                        arrayOfSlugs.push(slug);
-                    } 
-                })
-                .catch(error => console.log(error))
-                this.restOfArray = arrayOfSlugs;
-            }
+        if (arrayOfSlugs.length <=0){
+            fetch('https://picsum.photos/v2/list')
+            .then(res => {
+                return res.json()
+            })
+            .then(data =>  {
+                const arrayOfURLs = data.map(photo => {
+                    return photo.url;
+                });
+            
+                for (let i = 0; i < arrayOfURLs.length; i++) {
+                    let fullURL = arrayOfURLs[i];
+                    let slug = fullURL.slice(fullURL.lastIndexOf("/") + 1); 
+                    arrayOfSlugs.push(slug);
+                } 
+            })
+            .catch(error => console.log(error))
+            this.restOfArray = arrayOfSlugs;
+        }
     }
   }
 }
@@ -95,15 +95,19 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+main {
+    width: 100%;
+    height: 100vh;
+    background-color: black;
+}
 ul {
     list-style-type: none;
     padding: 0;
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: black;
     width: 100%;
-    height: 100vh;
+    height: 90%;
 }
 li {
   display: block;
@@ -113,22 +117,39 @@ li {
   &:nth-child(-n+3)
   {
       width: 33%;
+      height: 70%;
   }
 }
 a {
   color: #42b983;
 }
- #sliderView {
-    width: 100%;
-    height: 100%;
-}
-/* .slide {
-    width: 500px;
-    height: 500px;
-} */
 .imgInSlide{
     width: 100%;
-    height: auto;
+    height: 100%;
     object-fit: cover;
 }
+ #send
+    {
+        border: 0px;
+        width: 20%;
+        height: 50px;
+        outline: var(--background-beige);
+        color: black;
+        font-size: 1.5vw;
+        font-weight: bold;
+        /* background-size:1px 170px;  */
+       /*  background-image: radial-gradient(circle, rgb(199, 154, 153) 0%, rgb(208, 174, 164) 100%); */
+       background-image: url(~@/assets/wood.jpg);
+        display: inline-block;
+        cursor: pointer;
+        text-decoration: none;
+        text-shadow: black;
+        transition: all 0.1s;
+        &:active 
+        {
+            position: relative;
+            background-position: 0px 0.5px; 
+            top: 1px;
+        }
+    }
 </style>
